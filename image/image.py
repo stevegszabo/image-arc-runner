a2fcb582f29131448816d12af26f3de40d749dfd
Steve Szabo <steve.g.szabo@gmail.com>
2025-12-23 12:31:09 -0500

    Engineering (#3)
    * updates
    
    * workflow: updated image/image.yaml via github action
    
    * updates
    
    * updates
    
    * workflow: updated image/image.yaml via github action
    
    * updates
    
    * workflow: updated image/image.yaml via github action
    
    * updates
    
    * updates
    
    * workflow: updated image/image.yaml via github action
    
    * updates
    
    * workflow: updated image/image.yaml via github action
    
    * updates
    
    * workflow: updated image/image.yaml via github action
    
    * updates
    
    * workflow: updated image/image.yaml via github action
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- image/image.py
+++ image/image.py
@@ -11,10 +11,6 @@ def image_fetch(filename):
     with open(filename, "r", encoding="utf-8") as handle:
         content = yaml.safe_load(handle)
 
-    sys.stdout.write(f"content.repository: [{content['repository']}]\n")
-    sys.stdout.write(f"content.version: [{content['version']}]\n")
-    sys.stdout.write(f"content.release: [{content['release']}]\n")
-
     if os.getenv("GITHUB_OUTPUT"):
         delimiter = f"EOF-{uuid.uuid4()}"
         with open(os.getenv("GITHUB_OUTPUT"), "a", encoding="utf-8") as handle:
@@ -28,20 +24,17 @@ def image_update(filename):
     with open(filename, "r", encoding="utf-8") as handle:
         content = yaml.safe_load(handle)
 
-    elements = content["version"].split(".")
+    version = content.get("version", "0.0.0").split(".")
+    release = content.get("release", "patch").lower()
 
-    if content["release"] == "major":
-        elements[0] = str(int(elements[0]) + 1)
-    if content["release"] == "minor":
-        elements[1] = str(int(elements[1]) + 1)
-    if content["release"] == "patch":
-        elements[2] = str(int(elements[2]) + 1)
+    if release == "major":
+        version[0] = str(int(version[0]) + 1)
+    elif release == "minor":
+        version[1] = str(int(version[1]) + 1)
+    elif release == "patch":
+        version[2] = str(int(version[2]) + 1)
 
-    content["version"] = ".".join(elements)
-
-    sys.stdout.write(f"content.repository: [{content['repository']}]\n")
-    sys.stdout.write(f"content.version: [{content['version']}]\n")
-    sys.stdout.write(f"content.release: [{content['release']}]\n")
+    content["version"] = ".".join(version)
 
     with open(filename, "w") as handle:
         yaml.dump(content, handle)
@@ -50,16 +43,20 @@ def image_update(filename):
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="Generate image")
+    parser = argparse.ArgumentParser(description="Generate image version")
     parser.add_argument("--filename", dest="filename", default="image.yaml", help="image filename")
     parser.add_argument("--mode", dest="mode", default="fetch", help="operation mode")
     arguments = parser.parse_args()
 
     try:
         if arguments.mode.lower() == "update":
-            image_update(filename=arguments.filename)
+            content = image_update(filename=arguments.filename)
         else:
-            image_fetch(filename=arguments.filename)
+            content = image_fetch(filename=arguments.filename)
+
+        sys.stdout.write(f"content.repository: [{content['repository']}]\n")
+        sys.stdout.write(f"content.version: [{content['version']}]\n")
+        sys.stdout.write(f"content.release: [{content['release']}]\n")
     except Exception as err:
         sys.stderr.write(f"Unable to {arguments.mode.lower()} image: {err}\n")
         sys.exit(1)
