ba062b570951e7f29e44c0f32b96a23c6d16b6fd
Steve Szabo <steve.g.szabo@gmail.com>
2025-12-23 07:50:28 -0500

    updates

--- image/image.py
+++ image/image.py
@@ -13,29 +13,35 @@ def image_operation(filename, mode):
     else:
         return image_update(filename)
 
-def image_update(filename):
-    return {}
-
 def image_fetch(filename):
     with open(filename, "r", encoding="utf-8") as handle:
         content = yaml.safe_load(handle)
 
-    image = {
-        "version": content.get("version", "v0.0.0"),
-        "repository": content.get("repository", "repository")
-    }
-
-    sys.stdout.write(f"image.version: [{image['version']}]\n")
-    sys.stdout.write(f"image.repository: [{image['repository']}]\n")
+    sys.stdout.write(f"content.version: [{content['version']}]\n")
+    sys.stdout.write(f"content.repository: [{content['repository']}]\n")
 
     if os.getenv("GITHUB_OUTPUT"):
         delimiter = f"EOF-{uuid.uuid4()}"
         with open(os.getenv("GITHUB_OUTPUT"), "a", encoding="utf-8") as handle:
             handle.write(f"image<<{delimiter}\n")
-            handle.write(f"{json.dumps(image)}\n")
+            handle.write(f"{json.dumps(content)}\n")
             handle.write(f"{delimiter}\n")
 
-    return image
+    return content
+
+def image_update(filename):
+    with open(filename, "r", encoding="utf-8") as handle:
+        content = yaml.safe_load(handle)
+
+    content["version"] = "v0.0.1"
+
+    sys.stdout.write(f"content.version: [{content['version']}]\n")
+    sys.stdout.write(f"content.repository: [{content['repository']}]\n")
+
+    with open(filename, "w") as handle:
+        yaml.dump(content, handle)
+
+    return content
 
 if __name__ == "__main__":
 
