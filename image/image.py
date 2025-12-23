057dd70865ed2686ff7be0bfbcdd60105b45e9f6
Steve Szabo <steve.g.szabo@gmail.com>
2025-12-23 07:04:13 -0500

    updates

--- image/image.py
+++ image/image.py
@@ -3,32 +3,36 @@
 import os
 import sys
 import yaml
+import json
 import argparse
 
-def generate_image_version(filename):
+def generate_image(filename):
     with open(filename, "r", encoding="utf-8") as handle:
-        infrastructure = yaml.safe_load(handle)
+        content = yaml.safe_load(handle)
 
-    image_version = infrastructure.get("version", "v0.0.0")
+    image = {}
+    image.version = content.get("version", "v0.0.0")
+    image.repository = content.get("repository", "repository")
 
-    sys.stdout.write(f"image_version: [{image_version}]\n")
+    sys.stdout.write(f"image.version: [{image.version}]\n")
+    sys.stdout.write(f"image.repository: [{image.repository}]\n")
 
     if os.getenv("GITHUB_OUTPUT"):
         with open(os.getenv("GITHUB_OUTPUT"), "a", encoding="utf-8") as handle:
-            handle.write(f"image_version: '{image_version}'\n")
+            handle.write(f"image={json.dumps(image)}\n")
 
-    return image_version
+    return image
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="Generate image version")
+    parser = argparse.ArgumentParser(description="Generate image")
     parser.add_argument("--filename", dest="filename", default="image.yaml", help="image filename")
     arguments = parser.parse_args()
 
     try:
-        generate_image_version(filename=arguments.filename)
+        generate_image(filename=arguments.filename)
     except Exception as err:
-        sys.stderr.write(f"Unable to generate image version: {err}\n")
+        sys.stderr.write(f"Unable to generate image: {err}\n")
         sys.exit(1)
 
     sys.exit(0)
